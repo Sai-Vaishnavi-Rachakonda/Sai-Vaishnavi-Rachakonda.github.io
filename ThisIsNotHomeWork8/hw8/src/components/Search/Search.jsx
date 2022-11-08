7e002153f882588276a0f5fdb561bacdc65b65ef
Sai-Vaishnavi-Rachakonda <111935118+Sai-Vaishnavi-Rachakonda@users.noreply.github.com>
2022-11-07 20:38:15 -0800

    proxy added

--- ThisIsNotHomeWork8/hw8/src/components/Search/Search.jsx
+++ ThisIsNotHomeWork8/hw8/src/components/Search/Search.jsx
@@ -11,7 +11,7 @@ import { Form, InputGroup, Spinner } from 'react-bootstrap';
 
 function Search(props) {
 
-    const proxy = "http://localhost:8080/"
+    const proxy = "https://node-react-project-365904.wl.r.appspot.com/"
     const [keyWord, setkeyWord] = useState();
     const [kW, setkW] = useState();
     const [keyWordInput, setkeyWordInput] = useState();
